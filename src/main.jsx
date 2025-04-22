6ae84cc35a6d3ac96203ebf436a8d2a05c0bc8ed
atique-code <atiquekalwar123@gmail.com>
2025-04-22 21:51:23 +0500

    update by  toseef

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "../src/style/AuthStyle.css"
-import { HashRouter } from 'react-router-dom';
+// import "../src/style/AuthStyle.css"
+import './style/AuthStyle.css';
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+
     <App />
-    </HashRouter>
+  
   </StrictMode>,
 )
